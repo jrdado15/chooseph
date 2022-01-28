196941698381e60688c33ecd356572c76a62610a
Jus1006 <vergarajustine06@gmail.com>
2022-01-28 20:41:42 +0800

    rename files

--- dbconfig.php
+++ dbconfig.php
@@ -2,7 +2,7 @@
     $server = "localhost";
     $user = "root";
     $pass = "";
-    $db_name = "xdizhfxe_records";
+    $db_name = "xdizhfxe_bsit4b";
     $conn = mysqli_connect($server, $user, $pass, $db_name);
     if(!$conn){
         echo "Database connection error" . mysqli_connect_error();
